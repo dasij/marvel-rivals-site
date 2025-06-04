b9568e042b322c392bf0129059450d39ac9816f7
dasij <ahalanbw@hotmail.com>
2025-06-04 00:29:48 -0300

    started the website

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
+const webpack = require('webpack');
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/marvel-rivals-site/' : '/',
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+      }),
+    ],
+  },
 })
